6fa66ef697738d643bed0e348829b62c531e80ac
2410991101 <jasvin75.kamra@gmail.com>
2026-07-21 11:03:00 +0530

    Time: 6 ms (43.34%), Space: 44.3 MB (82.79%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;   // count for s
+            freq[t.charAt(i) - 'a']--;   // subtract for t
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
