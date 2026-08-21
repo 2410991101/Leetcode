392cf7abe1ec1e8df31eb5de4392cde1d45d3224
Jaisvin Kaur Kamra <jasvin75.kamra@gmail.com>
2026-08-21 11:36:13 +0530

    Time: 18 ms (67.57%), Space: 46.6 MB (30.71%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,55 +1,32 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        
         int n = nums.length;
-        
-        long minSum = 0;
-        long maxSum = 0;
-        
         Stack<Integer> st = new Stack<>();
-        
-        // Sum of subarray minimums
-        for(int i = 0; i <= n; i++) {
-            
-            while(!st.isEmpty() && 
-                  (i == n || nums[st.peek()] >= nums[i])) {
-                
+        long minsum=0;
+        long maxsum=0;
+        for(int i=0;i<=n;i++){
+            while(!st.isEmpty() && (i==n || nums[st.peek()]>=nums[i])){
                 int mid = st.pop();
-                
-                int left = st.isEmpty() ? -1 : st.peek();
+                int left = st.isEmpty()? -1: st.peek();
                 int right = i;
-                
-                long leftCount = mid - left;
-                long rightCount = right - mid;
-                
-                minSum += (long) nums[mid] * leftCount * rightCount;
+                long leftcount = mid-left;
+                long rightcount = right-mid;
+                minsum+= (long)nums[mid]*leftcount*rightcount;
             }
-            
             st.push(i);
         }
-        
         st.clear();
-        
-        // Sum of subarray maximums
-        for(int i = 0; i <= n; i++) {
-            
-            while(!st.isEmpty() && 
-                  (i == n || nums[st.peek()] <= nums[i])) {
-                
+        for(int i=0;i<=n;i++){
+            while(!st.isEmpty() && (i==n || nums[st.peek()]<=nums[i])){
                 int mid = st.pop();
-                
-                int left = st.isEmpty() ? -1 : st.peek();
+                int left = st.isEmpty()? -1: st.peek();
                 int right = i;
-                
-                long leftCount = mid - left;
-                long rightCount = right - mid;
-                
-                maxSum += (long) nums[mid] * leftCount * rightCount;
+                long leftcount = mid-left;
+                long rightcount = right-mid;
+                maxsum+= (long)nums[mid]*leftcount*rightcount;
             }
-            
             st.push(i);
         }
-        
-        return maxSum - minSum;
+        return maxsum-minsum;
     }
 }
